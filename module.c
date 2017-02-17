cf1a614e0546b2279981b520cfe0a3273403e9ac
green4free <ninjalostinsnow@gmail.com>
2017-02-17 09:51:03 +0100

    added C and Makefile

---
+++ module.c
@@ -0,0 +1,25 @@
+#include <linux/module.h>    // included for all kernel modules
+#include <linux/kernel.h>    // included for KERN_INFO
+#include <linux/init.h>      // included for __init and __exit macros
+#include <linux/i2c.h>       // included for i2c
+
+
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Magnus Östgren & Magnus Sörensen");
+MODULE_DESCRIPTION("");
+
+static int __init hello_init(void)
+{
+        printk(KERN_INFO "Hello world!\n");
+            return 0;    // Non-zero return means that the module couldn't be loaded.
+}
+
+static void __exit hello_cleanup(void)
+{
+        printk(KERN_INFO "Cleaning up module.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_cleanup);
